5f708fcc6fc9aa81a9bc67813d75a25a217644a8
Ariel Benichou <videojr3000@gmail.com>
2021-09-14 00:05:01 +0300

    added test for uniqueness of a million ids

---
+++ index.test.js
@@ -0,0 +1,47 @@
+const greg = require("./index.js");
+
+describe("uniqueness of million ids", () => {
+  const size = 1000000;
+  let smallestSetSize = size;
+  // we generate size new ids 3 times and take the worst case
+  for (let i = 0; i < 3; i++) {
+    const list = [];
+    for (let j = 0; j < size; j++) {
+      list.push(greg.sentence());
+    }
+    smallestSetSize = Math.min(new Set(list).size, smallestSetSize);
+  }
+
+  test("10% unique ids", () => {
+    const factor = 0.1;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("25% unique ids", () => {
+    const factor = 0.25;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("50% unique ids", () => {
+    const factor = 0.5;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("75% unique ids", () => {
+    const factor = 0.75;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("90% unique ids", () => {
+    const factor = 0.9;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("95% unique ids", () => {
+    const factor = 0.95;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("99% unique ids", () => {
+    const factor = 0.99;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+  test("100% unique ids", () => {
+    const factor = 1;
+    expect(smallestSetSize).toBeGreaterThanOrEqual(size * factor);
+  });
+});
